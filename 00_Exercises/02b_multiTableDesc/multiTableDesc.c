6449db3f6f1a12b34ef8b1b648763c177399ca0d
Roberto <roberto.may@stud.hslu.ch>
2020-03-05 20:24:42 +0100

    Aufgabe 2

---
+++ 00_Exercises/02b_multiTableDesc/multiTableDesc.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void PrintUsageMassage(void); // Funktionsprototyp
+void PrintMultiTable(int);
+
+const int lowerLimit = 1;
+const int uperLimit = 20;
+
+int main(int argc, char *argv[]){
+	
+	int inputValue = 0;
+
+	if( argc != 2){
+		PrintUsageMassage();
+		return 0;
+	}
+	else{
+		inputValue = atoi(argv[1]);
+		if( inputValue >= lowerLimit && inputValue <= uperLimit){
+			// input is valid
+			PrintMultiTable(inputValue);		
+		}
+		else{
+			// no valid input
+			PrintUsageMassage();
+		}
+	}
+
+
+	return 0;
+}
+
+void PrintUsageMassage(void){
+	printf("Usage: multiTable wholeNumber \n \twholeNumber: integer on the interval [%d,%d]\n",lowerLimit ,uperLimit);
+}
+
+void PrintMultiTable(int tableLimit){
+	for(int i = 1; i <= tableLimit; i++){
+		for(int j = tableLimit; j >= 1; j--){
+			printf("%d\t",i*j);
+		}
+		printf("\n");
+	}
+}
